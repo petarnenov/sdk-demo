bfd726218fb4ba744187ebc21953d85afa95d34b
Petar Nenov <petarnenovpetrov@gmail.com>
2023-03-03 16:47:19 +0200

    chore: remove from history dist

--- dist/base.d.ts
+++
@@ -1,9 +0,0 @@
-export type Config = {
-    apiKey: string;
-    baseUrl: string;
-};
-export declare abstract class Base {
-    private config;
-    constructor(config: Config);
-    protected fetch(path: string, options?: RequestInit): Promise<Response>;
-}
