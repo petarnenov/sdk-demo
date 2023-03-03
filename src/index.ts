a925f74603026da55f27d8e87881bd1f05806e7d
Petar Nenov <petarnenovpetrov@gmail.com>
2023-03-03 18:12:58 +0200

    feat: add sdk Post

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,8 @@ const helloWorld = () => {
   console.log("Hello World");
 };
 
-const post = new Post({
-  baseUrl: "https://jsonplaceholder.typicode.com",
-  apiKey: "",
-});
-
-post.getPosts().then((posts) => {
-  console.log(posts);
-});
-
 // Path: src/index.test.js
 export default {
   helloWorld,
+  Post
 };
